b0241c9b3bb6cc70818cb428e1fde70490ca6ea0
Chanatda Prasawang <64160045@go.buu.ac.th>
2024-12-03 23:07:47 +0700

    Add project files.

---
+++ AppQrCoder/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AppQrCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.date = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(210, 210);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(259, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 210);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Font = new System.Drawing.Font("TH Sarabun New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.Location = new System.Drawing.Point(13, 13);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(70, 36);
+            this.date.TabIndex = 3;
+            this.date.Text = "label1";
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("TH Sarabun New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(259, 13);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(71, 36);
+            this.time.TabIndex = 4;
+            this.time.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.buttonSave.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(259, 275);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(210, 40);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(481, 327);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ AppQrCoder/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AppQrCoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ตรวจสอบว่ามีข้อความใน RichTextBox หรือไม่
+                if (string.IsNullOrWhiteSpace(richTextBox1.Text))
+                {
+                    MessageBox.Show("กรุณากรอกข้อความเพื่อสร้าง QR Code");
+                    return;
+                }
+
+                // สร้าง QR Code
+                QRCoder.QRCodeGenerator qr = new QRCoder.QRCodeGenerator();
+                var MyData = qr.CreateQrCode(richTextBox1.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+                var code = new QRCoder.QRCode(MyData);
+
+                // สร้างภาพ QR Code
+                var qrImage = code.GetGraphic(10); // ปรับขนาดของโมดูล
+
+                // ตั้งค่าภาพใน PictureBox
+                pictureBox1.Image = qrImage;
+
+                // ปรับขนาด PictureBox ให้สัมพันธ์กับขนาดของภาพ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Width = 200; // ตั้งค่ากว้าง
+                pictureBox1.Height = 200; // ตั้งค่าสูง
+            }
+            catch (Exception ex)
+            {
+                // จัดการข้อผิดพลาด
+                MessageBox.Show("เกิดข้อผิดพลาด: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            date.Text = now.ToString("dd/MM/yyyy");
+            time.Text = now.ToString("HH:mm:ss");
+        }
+
+
+        private void buttonSave_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // ตรวจสอบว่ามี QR Code ใน PictureBox หรือไม่
+                if (pictureBox1.Image == null)
+                {
+                    MessageBox.Show("กรุณาสร้าง QR Code ก่อนการบันทึก");
+                    return;
+                }
+
+                // กำหนดเส้นทางการบันทึก
+                string folderPath = @"D:\Temp\QR"; // หรือสามารถเปลี่ยนเป็นพาธที่ต้องการ
+                string fileName = $"q_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.png";
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // ตรวจสอบว่าโฟลเดอร์สามารถเข้าถึงได้หรือไม่
+                if (!Directory.Exists(folderPath))
+                {
+                    MessageBox.Show("โฟลเดอร์ที่ระบุไม่มีอยู่หรือไม่สามารถเข้าถึงได้");
+                    return;
+                }
+
+                // บันทึกภาพ QR Code เป็นไฟล์ PNG
+                pictureBox1.Image.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+
+                // แสดงข้อความเมื่อบันทึกสำเร็จ
+                MessageBox.Show($"QR Code ได้ถูกบันทึกไว้ที่ {filePath}");
+            }
+            catch (Exception ex)
+            {
+                // แสดงข้อความข้อผิดพลาด
+                MessageBox.Show("เกิดข้อผิดพลาด: " + ex.Message);
+            }
+        }
+    }
+}
